4c7d326575e01514b44508205f87d429172a2d48
github username <github email address>
2021-05-16 19:30:22 -0400

    created index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,22 @@ class UsersController < ApplicationController
     get "/signup" do 
         erb :"users/create_user"
     end
-    
+
+    post "/signup" do
+        user=User.new(name: params[:name], email: params[:email], password: params[:password])
+
+        if  @user.save && !@user.username.empty? && !@user.email.empty? 
+            
+            session[:user_id] = @user.id
+
+            redirect "/recipes"
+        else
+            redirect "/signup"
+        end
+
+     end
+
+     
+
+
 end
