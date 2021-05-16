477a65ea07e8ada4c1d2bdbae0f6f02f760b8153
github username <github email address>
2021-05-16 14:01:36 -0400

    created application_controller and index route

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, Proc.new { File.join(root, "../views/") }
+    enable :sessions
+		set :session_secret, "password_security"
+  end
+
+  get "/" do 
+    erb :"index"
+  end
+end
