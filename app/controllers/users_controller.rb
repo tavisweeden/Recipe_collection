22e6a295afec6c0e5086267b05e2d7040092440f
github username <github email address>
2021-05-22 16:15:27 -0400

    created login route and login view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
     get "/signup" do 
-        erb :"users/create_user"
+        erb :'users/create_user'
     end
 
     post "/signup" do
@@ -11,14 +11,32 @@ class UsersController < ApplicationController
             
             session[:user_id] = @user.id
 
-            redirect "/"
+            redirect "/recipes"
         else
             redirect "/signup"
         end
 
      end
 
-     
+     get "/login" do
+        erb :'/login'
+     end
 
+     post "/login" do
+        @user = User.find_by(:name => params[:name])
 
+        if @user && @user.authenticate(params[:password])
+
+            session[:user_id] = @user.id
+        
+            redirect "/recipes" # or redirect "/failure"
+
+        else
+
+            redirect "/login"
+
+        end
+
+     end
+    
 end
